7cd59ec66a0ba68e4d21e219c2f3af4db64c753d
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2026-01-09 14:29:21 +0100

    CF UPDATES

--- exceptions/src/CodiceFiscale.java
+++ exceptions/src/CodiceFiscale.java
@@ -7,7 +7,25 @@ public class CodiceFiscale {
     public CodiceFiscale(String cf) throws CodiceFiscaleNonValidoException {
         if (cf.length() != MAX_LENGTH) throw new CodiceFiscaleNonValidoException("CF NON VALIDO");
 
+        /*
+         *1) for + charAt()
+         * 2) usa il vettore dei caraterri più controllo
+         * 3) isDigitOrLetter()
+         */
+        for (int i = 0; i < cf.length(); i++) {
+            if (!isLetter(cf.charAt(i)) && !Character.isDigit(cf.charAt(i))) {
+                throw new CodiceFiscaleNonValidoException("CF non alfanumerico");
+            }
+        }
+
+        if (!Character.isUpperCase(cf.charAt(MAX_LENGTH - 1)))
+            throw new CodiceFiscaleNonValidoException("ctrl char NON MAIUSCOLO");
+
         this.cf = cf;
+    }
+
+    private boolean isLetter(char c) {
+        return (c >= 65 && c <= 90) || (c >= 97 && c <= 122);
     }
 
     @Override
--- exceptions/src/Main.java
+++ exceptions/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         try {
-            CodiceFiscale cf = new CodiceFiscale("aaaaaaaaaaaaaaaa");
+            CodiceFiscale cf = new CodiceFiscale("zxcvasfdEWRQT123");
             System.out.println(cf + " VALIDO");
         } catch (CodiceFiscaleNonValidoException e) {
             System.out.println(e.getMessage());
