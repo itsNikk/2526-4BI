d0d6dfc83073eceaa59224ba80500f210e2683f3
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2026-01-16 13:22:09 +0100

    Update Main.java

--- exceptions/src/Probability/Main.java
+++ exceptions/src/Probability/Main.java
@@ -1,8 +1,21 @@
 package Probability;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         // Math.random vs new Random();
-                   
+        // /dev/urandom o /dev/random
+        Random r = new Random();
+        for (int i = 0; i < 100000; i++) {
+            double val = r.nextDouble(); // [0.0, 1.00)
+
+            System.out.println(val);
+
+            if (val <= 0.3) {
+                throw new RuntimeException("BOOM");
+            }
+
+        }
     }
 }
