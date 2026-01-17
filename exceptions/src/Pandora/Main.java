cb65c23900d83f980ee82c2479301c201ceffe8d
iTzNikk <nicolo.buganza@tiscali.it>
2026-01-17 16:15:18 +0100

    Update Main.java

--- exceptions/src/Pandora/Main.java
+++ exceptions/src/Pandora/Main.java
@@ -5,12 +5,14 @@ public class Main {
         try {
             Coordinata c = new Coordinata(16516, 468468);
             Arma z = new Arma();
-            Avatar a = new Avatar("dnfjf", new Arma(), 123);
+            Avatar a = new Avatar("dnfjf", z, 123);
             a.attacca(new PostazioneRDA());
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-        } catch (ArmaMalfunzionanteException a) {
-
+        } catch (ArmaMalfunzionanteException armaMalfunzionanteException) {
+            System.out.println(armaMalfunzionanteException.getMessage());
+        } catch (AttccoFallitoException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
