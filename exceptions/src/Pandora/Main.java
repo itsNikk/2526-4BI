c31197abfbcc81b4cf332bcbc5927d161090a0ef
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2026-01-23 14:15:43 +0100

    dsaffgdsasafd

--- exceptions/src/Pandora/Main.java
+++ exceptions/src/Pandora/Main.java
@@ -11,6 +11,8 @@ public class Main {
             System.out.println(e.getMessage());
         } catch (ArmaMalfunzionanteException a) {
 
+        } catch (AttccoFallitoException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
