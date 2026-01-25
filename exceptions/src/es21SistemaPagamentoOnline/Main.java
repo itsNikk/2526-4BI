1a2c41729ddc4c4996cd20fba95d082c182886fa
iTzNikk <nicolo.buganza@tiscali.it>
2026-01-25 12:53:32 +0100

    Create Main.java

---
+++ exceptions/src/es21SistemaPagamentoOnline/Main.java
@@ -0,0 +1,39 @@
+package es21SistemaPagamentoOnline;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("=== SISTEMA DI PAGAMENTO E-COMMERCE ===\n");
+
+        // Creazione carte di test
+        CartaDiCredito cartaScaduta = new CartaDiCredito("4532123456789012", 11, 2023, "123", 500.00);
+
+        CartaDiCredito cartaSaldoBasso = new CartaDiCredito("5425233430109903", 8, 2027, "456", 15.00);
+
+        CartaDiCredito cartaValida = new CartaDiCredito("4716258050958645", 12, 2028, "789", 1000.00);
+
+        ProcessorePagamenti processore = new ProcessorePagamenti();
+
+        // Scenario di test
+        System.out.println("Tentativo 1: Pagamento con carta scaduta");
+        processore.effettuaPagamento(cartaScaduta, 50.00);
+
+        System.out.println("\nTentativo 2: Pagamento che supera il saldo");
+        processore.effettuaPagamento(cartaSaldoBasso, 100.00);
+
+        System.out.println("\nTentativo 3-7: Pagamenti con carta valida (random esiti)");
+        for (int i = 0; i < 15; i++) {
+            boolean esito = processore.effettuaPagamento(cartaValida, 50.00);
+
+            if (esito) {
+                System.out.println("Tentativo " + (i + 1) + ": Successo");
+            } else System.out.println("Tentativo " + (i + 1) + ": Fallito");
+
+            // Piccola pausa per rendere più leggibile l'output
+            Thread.sleep(500);
+        }
+
+        processore.printTransactionLog();
+
+        System.out.println("Saldo finale carta valida: €" + cartaValida.getSaldo());
+    }
+}
