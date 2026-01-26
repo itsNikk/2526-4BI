4735c23bf7aa4ceb441a13fd3a9b03c94af37a49
iTzNikk <nicolo.buganza@tiscali.it>
2026-01-26 21:44:09 +0100

    Create HotelMain.java

---
+++ exceptions/src/es3HotelMultiLivello/HotelManagement/HotelMain.java
@@ -0,0 +1,9 @@
+package es3HotelMultiLivello.HotelManagement;
+
+public class HotelMain {
+    public static void main(String[] args) {
+        DatabaseSimulato databaseSimulato = new DatabaseSimulato();
+        GestorePrenotazioni gestorePrenotazioni = new GestorePrenotazioni();
+        InterfacciaHotel interfacciaHotel = new InterfacciaHotel();
+    }
+}
