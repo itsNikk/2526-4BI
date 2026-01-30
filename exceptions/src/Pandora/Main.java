50e87943c0f9175ee606fcc42afdeefd01b98cbe
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2026-01-30 14:00:18 +0100

    Update Main.java

--- exceptions/src/Pandora/Main.java
+++ exceptions/src/Pandora/Main.java
@@ -1,18 +1,27 @@
 package Pandora;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            Coordinata c = new Coordinata(46, 85);
-            Arma z = new Arma();
-            Avatar a = new Avatar("dnfjf", z, 123);
-            a.attacca(new PostazioneRDA());
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        } catch (ArmaMalfunzionanteException armaMalfunzionanteException) {
-            System.out.println(armaMalfunzionanteException.getMessage());
-        } catch (AttccoFallitoException e) {
-            System.out.println(e.getMessage());
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 30; i++) {
+            try {
+                Coordinata c = new Coordinata(46, 85);
+                Arma z = new Arma();
+                Avatar a = new Avatar("Jake", z, 123);
+
+
+                a.attacca(new PostazioneRDA());
+                System.out.println("avatar attacca");
+
+                // Fa dormire il programma per 500 ms
+                Thread.sleep(500);
+
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            } catch (ArmaMalfunzionanteException armaMalfunzionanteException) {
+                System.out.println(armaMalfunzionanteException.getMessage());
+            } catch (AttccoFallitoException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
