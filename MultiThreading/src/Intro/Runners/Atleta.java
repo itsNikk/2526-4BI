568871cbe33c0d6fb433f0f426931a63cd6ee1c1
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2026-02-06 14:34:00 +0100

    intro thread

--- MultiThreading/src/Intro/Runners/Atleta.java
+++ MultiThreading/src/Intro/Runners/Atleta.java
@@ -15,6 +15,13 @@ public class Atleta extends Thread {
         startTime = System.currentTimeMillis();
         for (int i = 0; i < 10; i++) {
             System.out.println(getName() + "Step " + (i + 1));
+            try {
+                sleep(300 + (int) (Math.random() * 500));
+            } catch (InterruptedException e) {
+                //SI BENE.. e ora?
+                System.out.println(getName() + " interrotto");
+                break;
+            }
         }
 
         endTime = System.currentTimeMillis();
--- MultiThreading/src/Intro/Runners/Main.java
+++ MultiThreading/src/Intro/Runners/Main.java
@@ -1,11 +1,29 @@
 package Intro.Runners;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         Atleta a = new Atleta("Andrea");
+        Atleta b = new Atleta("Chiara");
 
-        
+        a.start();
+        b.start();
+
+        /*
+        boolean toEliminate = new Random().nextBoolean();
+        if (toEliminate) a.interrupt();
+        else b.interrupt();
+*/
+        try {
+            a.join();
+            b.join();
+        } catch (InterruptedException c) {
+            System.out.println(c.getMessage());
+        }
 
         System.out.println(a.getName() + ": TEMPO:" + a.getRaceTime());
+        System.out.println(b.getName() + ": TEMPO:" + b.getRaceTime());
+
     }
 }
