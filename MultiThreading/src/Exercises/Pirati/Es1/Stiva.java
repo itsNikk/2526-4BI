119d5b788a3fb216adead3a763bb82e7e8e97778
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2026-03-13 13:38:24 +0100

    Update Stiva.java

--- MultiThreading/src/Exercises/Pirati/Es1/Stiva.java
+++ MultiThreading/src/Exercises/Pirati/Es1/Stiva.java
@@ -2,6 +2,31 @@ package Exercises.Pirati.Es1;
 
 import java.util.ArrayList;
 
+//Shared resource...
 public class Stiva {
     private ArrayList<LootItem> stiva;
+    private final int MAX_SIZE;
+
+    public Stiva(int maxSize) {
+        this.stiva = new ArrayList<>();
+        this.MAX_SIZE = maxSize;
+    }
+
+    public synchronized void depositLoot(LootItem lootItem) throws InterruptedException {
+        while (stiva.size() == MAX_SIZE) wait();
+        stiva.add(lootItem);
+        notifyAll();
+    }
+
+    public synchronized LootItem getLoot() throws InterruptedException {
+        while (stiva.isEmpty()) wait();
+
+        //FIFO....
+        LootItem toGetBack = stiva.remove(0);
+        notifyAll();
+        return toGetBack;
+
+    }
+
+
 }
