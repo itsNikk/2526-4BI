34938d695593f2e36d5a6f8fa5bb736aaca4c0c1
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2026-03-13 14:07:53 +0100

    Update Main.java

--- MultiThreading/src/Exercises/Pirati/Es1/Main.java
+++ MultiThreading/src/Exercises/Pirati/Es1/Main.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        final int MAX_PIRATES = 20;
-        Stiva stiva = new Stiva(5);
+        final int MAX_PIRATES = 3;
+        Stiva stiva = new Stiva(30);
 
         //Threads...
         ArrayList<Pirata> pirates = new ArrayList<>();
@@ -13,5 +13,7 @@ public class Main {
         for (int i = 0; i < MAX_PIRATES; i++) {
             pirates.add(new Pirata(stiva, "Pirata-" + (i + 1)));
         }
+
+        for (Pirata p : pirates) p.start();
     }
 }
